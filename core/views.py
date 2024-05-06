dd9256f840939f15ff6308a5578c5f219417c55b
Avni Goel <avnigoel98@gmail.com>
2024-05-06 20:35:36 +0530

    worked on portfolio website

--- core/views.py
+++ core/views.py
@@ -1,6 +1,25 @@
+# from django.shortcuts import render
+
+# # Create your views here.
+
+# def home(request):
+#     return render (request, 'home.html')
+
+
+
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import *
 
-# Create your views here.
 
-def home(request):
-    return render (request, 'home.html')
+class HomeTemplateView(TemplateView):
+    template_name = 'home.html'
+
+    # override get context date method
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)  # first, call super get context data
+        context['about'] = About.objects.first()
+        context['services'] = Service.objects.all()
+        context['works'] = RecentWork.objects.all()
+        context['clients'] = Client.objects.all()
+        return context
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -118,17 +118,26 @@ USE_TZ = True
 
 import os
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIR =  {
-    os.path.join(BASE_DIR , "public/static")
-}
+# STATICFILES_DIR =  {
+#     os.path.join(BASE_DIR , "public/static")
+# }
 
-MEDIA_ROOT = os.path.join(BASE_DIR , 'public/static')
-MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR , 'public/static')
+# MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
